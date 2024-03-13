3b452d1e67fd3995bf87c8c2756ee062dddff6ad
jhe137 <jhe137@gmail.com>
2024-03-13 18:07:43 +1100

    feat: horizontal variation of hero

--- src/slices/Hero/index.tsx
+++ src/slices/Hero/index.tsx
@@ -8,7 +8,7 @@ import Heading from "@/components/Heading";
 const components: JSXMapSerializer = {
   heading1:({children}) => (<Heading as ="h1" size ="xl" className="md:mb-8 mb-4 mt-12 first:mt-0 last:mb-0">{children}</Heading>),
   
-  paragraph: ({ children }) => <p className="text-2xl text-center font-normal leading-10 font-body text-slate-600 mb-4 md:mb-8 max-w-md">{children}</p>
+  paragraph: ({ children }) => <p className="text-2xl font-normal leading-10 font-body text-slate-600 mb-4 md:mb-8 max-w-md">{children}</p>
   
   }
 
@@ -23,6 +23,9 @@ export type HeroProps = SliceComponentProps<Content.HeroSlice>;
  */
 const Hero = ({ slice }: HeroProps): JSX.Element => {
   return (
+
+    <>
+    {slice.variation ==="default" &&(
     <Bounded
     
       data-slice-type={slice.slice_type}
@@ -40,7 +43,31 @@ const Hero = ({ slice }: HeroProps): JSX.Element => {
     <Button field={slice.primary.button_link} className="mb-8 md:mb-10">{slice.primary.button_text}</Button>
     <PrismicNextImage field={slice.primary.image} className="drop-shadow-xl max-w-4xl w-full"/>
 
-    </div></Bounded> 
+    </div></Bounded> )}
+
+    {slice.variation ==="horizontal" &&(
+    <Bounded
+    
+      data-slice-type={slice.slice_type}
+      data-slice-variation={slice.variation}
+    >
+      
+        <div className="grid grid-cols-1 md:grid-cols-2 place-items-center">
+          <div className="grid grid-rows-[1fr,auto,auto] h-fit">
+    <PrismicRichText field={slice.primary.heading} 
+    components= {components}
+    />
+    <PrismicRichText field={slice.primary.body} 
+    components= {components}
+    />
+    
+    <Button field={slice.primary.button_link} className="mb-8 md:mb-10">{slice.primary.button_text}</Button>
+    </div>  
+
+    <PrismicNextImage field={slice.primary.image} className="drop-shadow-xl max-w-4xl w-full"/>
+
+    </div></Bounded> )}
+    </>
      );
 };
 
